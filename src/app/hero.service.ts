52f738d116a8128a426343d074541c28e0d840de
Sanker das <sankerdas.yes@gmail.com>
2021-09-28 20:43:24 +0530

    Add services - Show messages
    message.component
    message.service  are created
    message component added into app.component.html
    
    message.service initialized in the constructor of the hero.service and messages.component
    
    whenever messages[] contains value notification will be displayed
    
    messages[] controlled by add(), clear() methods inside the message.service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,15 +5,18 @@ import { HEROES } from './mock-heroes'; // mock data
 
 import { Observable, of } from 'rxjs'
 
+import { MessageService } from './message.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor( private messageService: MessageService ) { }
 
   getHeroes(): Observable<Hero[]> {
     const heros = of(HEROES);
+    this.messageService.add("HeroService: fetched heroes")
     return heros
   }
 }
