3e261e64535cfb954bf2143127819d5d35e7efb6
Sanker das <sankerdas.yes@gmail.com>
2021-09-28 22:04:07 +0530

    Add services - Add additional messages to hero service
    message service added into heroes.component to show the selected heroes list on notification message.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,6 +3,8 @@ import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
 
+import { MessageService } from '../message.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -11,7 +13,7 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
-  constructor( private heroService: HeroService ) { }
+  constructor( private heroService: HeroService, private messageService: MessageService ) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -27,6 +29,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect( hero: Hero ): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroComponent: Selected Hero id is = ${hero.id}`)
   }
 
 }
