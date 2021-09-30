03ce8c8dbad33b48f24437992768e85f883a7674
Sanker das <sankerdas.yes@gmail.com>
2021-09-30 12:48:09 +0530

    Get data from a server - Heroes and HTTP
    1. import http into hero.service.ts
    2. define api url of the form
    :base/:collectionName
    private heroesUrl = 'api/heroes';
    3. fetch data using http method
    4. error handling
    import { catchError, map, tap } from 'rxjs/operators';
    import above things for error handling
    add .pipe for exception handling
    5. handleError() is created to handle error
    6. log() is created to display logs

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -7,26 +7,53 @@ import { Observable, of } from 'rxjs'
 
 import { MessageService } from './message.service';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor( private messageService: MessageService ) { }
+  constructor( private http: HttpClient, private messageService: MessageService ) { }
+
+  private heroesUrl = 'api/heroes'; // InMemoryData plugin format format : base/:collectionName
 
   // Get all heroes
   getHeroes(): Observable<Hero[]> {
-    const heros = of(HEROES);
-    this.messageService.add("HeroService: fetched heroes")
-    return heros
+
+    // Mock server (similar to http server) response
+    // it returns an observable
+    return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      tap(_ => this.log('fetched heroes')), // WHAT is tap ??? I dont't know, FIND IT
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    ) // pipe used for observables error handling
+
+    // Mock data
+    // const heros = of(HEROES);
+    // this.messageService.add("HeroService: fetched heroes")
+
   }
 
   // Get specific hero
   getHero(id: number): Observable<Hero> {
-  // For now, assume that a hero with the specified `id` always exists.
-  // Error handling will be added in the next step of the tutorial.
     const hero = HEROES.find(elm => elm.id === id)!;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
   }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`)
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
 }
